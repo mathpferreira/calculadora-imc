78ff1a1fc52f46ae1449a4cdbfd2527438a53d99
Matheus Ferreira <mxthferreira@gmail.com>
2025-06-22 21:03:57 -0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function calcularIMC() {
   const peso = parseFloat(document.getElementById('peso').value);
   const altura = parseFloat(document.getElementById('altura').value);
@@ -45,43 +44,3 @@ document.addEventListener("keydown", function (event) {
     calcularIMC();
   }
 });
-=======
-function calcularIMC() {
-      const peso = parseFloat(document.getElementById('peso').value);
-      const altura = parseFloat(document.getElementById('altura').value);
-      const resultado = document.getElementById('resultado');
-
-      if (!peso || !altura || altura <= 0) {
-        resultado.textContent = "Por favor, insira valores válidos.";
-        resultado.className = "mt-6 text-center text-lg font-medium text-red-500";
-        return;
-      }
-
-      const imc = peso / (altura * altura);
-      let classificacao = "";
-      let corClasse = "text-gray-800";
-
-      if (imc < 18.5) {
-        classificacao = "Abaixo do peso";
-        corClasse = "text-red-500";
-      } else if (imc < 24.9) {
-        classificacao = "Peso ideal 👏";
-        corClasse = "text-green-600";
-      } else if (imc < 29.9) {
-        classificacao = "Sobrepeso";
-        corClasse = "text-red-500";
-      } else {
-        classificacao = "Obesidade";
-        corClasse = "text-red-500";
-      }
-
-      resultado.className = `mt-6 text-center text-lg font-medium ${corClasse}`;
-      resultado.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong> (${classificacao})`;
-    }
-
-    document.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    calcularIMC();
-  }
-});
->>>>>>> 0515058708b677ef0fcd6d307e7dbd2e10e7413e
